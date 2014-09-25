8a6bb37cd39b9a1ce1f65ea538122830564ecb99
laboshinl <laboshinl@laboshinl.stu.neva.ru>
2014-09-25 17:58:17 +0400

    first commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "firewalld"
+maintainer       "Martin Janser"
+maintainer_email "martin@gogan.ch"
+license          "Apache 2.0"
+description      "Install and configure firewalld"
+version          "0.2.0"
+
+supports "centos", ">= 7.0"
---
+++ recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: firewalld
+# Recipe:: default
+#
+# Copyright 2013, Martin Janser <martin@gogan.ch>
+#
+# Apache 2.0 License.
+#
+
+package 'firewalld'
+
+service 'firewalld' do
+  service_name 'firewalld.service'
+  provider Chef::Provider::Service::Systemd
+  action [:enable, :start] 
+end
