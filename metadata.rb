291892d36d041b54e93ffd498eaf047095ce98ec
Leonid Laboshin <laboshinl@gmail.com>
2014-09-26 17:39:34 +0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       "Martin Janser"
 maintainer_email "martin@gogan.ch"
 license          "Apache 2.0"
 description      "Install and configure firewalld"
-version          "0.2.0"
+version          "0.2.1"
 
 supports "centos", ">= 7.0"
